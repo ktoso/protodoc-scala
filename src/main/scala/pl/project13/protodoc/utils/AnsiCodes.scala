d36069cc543a91ec78ba2def2ce34b21d6242412
Konrad Malawski <konrad.malawski@project13.pl>
2011-10-31 19:13:46 +0100

    improved colors, made it work under linux console

--- src/main/scala/pl/project13/protodoc/utils/AnsiCodes.scala
+++ src/main/scala/pl/project13/protodoc/utils/AnsiCodes.scala
@@ -5,31 +5,23 @@ package pl.project13.protodoc.utils
  */
 trait AnsiCodes {
 
-  val BLACK = ANSI("0;30")
-  val BLUE = ANSI("0;34")
-  val GREEN = ANSI("0;32")
-  val CYAN = ANSI("0;36")
-  val RED = ANSI("0;31")
-  val PURPLE = ANSI("0;35")
-  val BROWN = ANSI("0;33")
-  val GRAY = ANSI("0;37")
-  val DARK_GRAY = ANSI("1;30")
-  val LIGHT_BLUE = ANSI("1;34")
-  val LIGHT_GREEN = ANSI("1;32")
-  val LIGHT_CYAN = ANSI("1;36")
-  val LIGHT_RED = ANSI("1;31")
-  val LIGHT_PURPLE = ANSI("1;35")
-  val YELLOW = ANSI("1;33")
-  val WHITE = ANSI("1;37")
+  val BLACK        = "\u001B[0;30m"
+  val BLUE         = "\u001B[0;34m"
+  val GREEN        = "\u001B[0;32m"
+  val CYAN         = "\u001B[0;36m"
+  val RED          = "\u001B[0;31m"
+  val PURPLE       = "\u001B[0;35m"
+  val BROWN        = "\u001B[0;33m"
+  val GRAY         = "\u001B[0;37m"
+  val DARK_GRAY    = "\u001B[1;30m"
+  val LIGHT_BLUE   = "\u001B[1;34m"
+  val LIGHT_GREEN  = "\u001B[1;32m"
+  val LIGHT_CYAN   = "\u001B[1;36m"
+  val LIGHT_RED    = "\u001B[1;31m"
+  val LIGHT_PURPLE = "\u001B[1;35m"
+  val YELLOW       = "\u001B[1;33m"
+  val WHITE        = "\u001B[1;37m"
+  val BOLD         = "\u001B[1m"
+  val RESET        = "\u001B[0m"
 
-  val BOLD = ANSI(1)
-  val RESET = ANSI(0)
-
-  def ANSI(style: Any, value: Any) {
-    ANSI(style + ";" + value)
-  }
-
-  def ANSI(value: Any) {
-    "\u001B[" + value + "m"
-  }
 }
